9b4ef69945517f3a8d7fdc8aa2739cecd0d48c7a
Dobby-yhs <jjj1299@naver.com>
2025-04-07 15:35:39 +0900

    Null 조건부 연산자

--- MainApp.cs
+++ MainApp.cs
@@ -1,62 +1,33 @@
 ﻿using System;
+using System.Collections;
+using System.Globalization;
 using static System.Console;
 
-namespace StringSlice
+namespace NullConditionalOperator
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-            string greeting = "Hello  World";
-            string[] arr1 = greeting.Split(new string[] { " " }, StringSplitOptions.None);
-            // arr1: ["Hello", "", "World"]
-            foreach (string element in arr1)
-                WriteLine("{0}", element);
+            ArrayList a = null;
 
-            WriteLine();
-            WriteLine();
+            a?.Add("야구");  // a?.가 null을 반환하므로 Add() 메소드는 호출되지 않습니다.
+            a?.Add("축구");
 
-            string[] arr2 = greeting.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-            // arr2: ["Hello", "World"]
-            foreach (string element in arr2)
-                WriteLine("{0}", element);
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
 
-            WriteLine();
-            WriteLine();
 
-            string testString = "aaVaaaVVaaaa";
-            string[] arr3 = testString.Split(new string[] {"V"}, StringSplitOptions.None);
-            foreach (string element in arr3)
-                WriteLine("{0}", element);
 
-            WriteLine();
-            WriteLine();
+            a = new ArrayList();  // a는 이제 더 이상 null이 아닙니다.
 
-            string[] arr4 = testString.Split(new string[] { "V" }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string element in arr4)
-                WriteLine("{0}", element);
+            a?.Add("야구");
+            a?.Add("축구");
 
-            WriteLine();
-            WriteLine();
-
-            string[] arr5 = testString.Split(new string[] { "bb" }, StringSplitOptions.None);
-            foreach (string element in arr5)
-                WriteLine("{0}", element);
-
-            WriteLine();
-            WriteLine();
-
-            string[] arr6 = testString.Split("b", StringSplitOptions.None);
-            foreach (string element in arr6)
-                WriteLine("{0}", element);
-
-            WriteLine();
-            WriteLine();
-
-            string[] arr7 = testString.Split("V");
-            foreach (string element in arr7)
-                WriteLine("{0}", element);
-
+            WriteLine($"Count : {a?.Count}");
+            WriteLine($"{a?[0]}");
+            WriteLine($"{a?[1]}");
         }
     }
 }
