71dec1ce990bedbca42984c336976b88a593232f
Dobby-yhs <jjj1299@naver.com>
2025-04-19 22:36:30 +0900

    Polymorphism Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,28 +1,31 @@
 ﻿using System;
 
-namespace TypeCasting
+namespace Overriding
 {
-    class Mammal
+    class ArmorSuite
     {
-        public void Nurse()
+        public virtual void Initialize()
         {
-            Console.WriteLine("Nurse()");
+            Console.WriteLine("Aromored");
         }
     }
 
-    class Dog : Mammal
+    class IronMan : ArmorSuite
     {
-        public void Bark()
+        public override void Initialize()
         {
-            Console.WriteLine("Bark()");
+            base.Initialize();
+            Console.WriteLine("Repulsor Rays Armed");
         }
     }
 
-    class Cat : Mammal
+    class WarMachine : ArmorSuite
     {
-        public void Meow()
+        public override void Initialize()
         {
-            Console.WriteLine("Meow()");
+            base.Initialize();
+            Console.WriteLine("Double-Barrel Cannons Armed");
+            Console.WriteLine("Micro-Rocket Launcher Armed");
         }
     }
 
@@ -30,35 +33,17 @@ namespace TypeCasting
     {
         static void Main(string[] args)
         {
-            Mammal mammal = new Dog();
-            Dog dog;
+            Console.WriteLine("Creating ArmorSuite...");
+            ArmorSuite armoursuite = new ArmorSuite();
+            armoursuite.Initialize();
 
-            if (mammal is Dog)  // is 연산자를 사용하여 타입 검사 수행
-            {
-                dog = (Dog)mammal;  // 명시적 형 변환(casting)으로 Mammal 타입의 mammal 객체를 Dog 타입으로 변환합니다. 
-                                    // 이전에 is 연산자를 통해 mammal이 Dog 타입임을 확인했기 때문에 안전합니다.
-                dog.Bark();
-            }
+            Console.WriteLine("\nCreating IronMan...");
+            ArmorSuite ironman = new IronMan();
+            ironman.Initialize();
 
-            Mammal mammal2 = new Cat();
-
-            Cat cat = mammal2 as Cat;  // as 연산자를 사용하여 형 변환
-            if (cat != null)
-            {
-                cat.Meow();
-            }
-
-            Cat cat2 = mammal as Cat;  // as 연산자를 사용하여 mammal 객체를 Cat 타입으로 변환 시도합니다. 
-                                       // 만약 변환이 불가능하면 null이 반환됩니다.
-
-            if (cat2 != null)
-            {
-                cat2.Meow();
-            }
-            else
-            {
-                Console.WriteLine("cat2 is not a Cat");
-            }
+            Console.WriteLine("\nCreating WarMachine...");
+            ArmorSuite warmachine = new WarMachine();
+            warmachine.Initialize();
         }
     }
 }
