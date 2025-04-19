3235d627e73f23d4398718be04643d6a60dd2db5
Dobby-yhs <jjj1299@naver.com>
2025-04-19 23:20:12 +0900

    Readonly Field Practice : Check Compile Error

--- MainApp.cs
+++ MainApp.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 
-class Base
+namespace ReadonlyFields
 {
-    public virtual void SealMe() { }
-}
-
-class Derived : Base
-{
-    public sealed override void SealMe() { }
-}
-
-class WantToOverride : Derived
-{
-    public override void SealMe() { }  // 컴파일 에러 발생
-}
-
-class MainApp
-{
-    static void Main(string[] args)
+    class Configuration
     {
+        // readonly를 이용해서 읽기 전용 필드를 선언합니다.
+        private readonly int min;
+        private readonly int max;
 
+        public Configuration(int v1, int v2)
+        {
+            // 읽기 전용 필드는 생성자 안에서만 초기화 가능합니다.
+            min = v1;
+            min = v2;
+        }
+
+        public void ChangeMax(int newMax)
+        {
+            // 생성자가 아닌 다른 곳에서 값을 수정하려하면 컴파일 에러가 발생합니다.
+            max = newMax;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Configuration c = new Configuration(100, 10);
+        }
     }
 }
