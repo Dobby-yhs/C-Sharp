5c2adcc2c6dddeaa9381cd3b12a0a61d58383d92
Dobby-yhs <jjj1299@naver.com>
2025-04-25 17:23:41 +0900

    Auto Implemented Property Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,41 +1,16 @@
 ﻿using System;
 
-namespace Property
+namespace AutoImplementedProperty
 {
     class BirthdayInfo
     {
-        private string name;
-        private DateTime birthday;
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-            set
-            {
-                name = value;
-            }
-        }
-
-        public DateTime Birthday
-        {
-            get
-            {
-                return birthday;
-            }
-            set
-            {
-                birthday = value;
-            }
-        }
-
+        public string Name { get; set; } = "UnKnown";
+        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
         public int Age
         {
             get
             {
-                return new DateTime(DateTime.Now.Subtract(birthday).Ticks).Year;
+                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
             }
         }
     }
@@ -45,6 +20,10 @@ namespace Property
         static void Main(string[] args)
         {
             BirthdayInfo birth = new BirthdayInfo();
+            Console.WriteLine($"Name : {birth.Name}");
+            Console.WriteLine($"Birthday : {birth.Birthday.ToShortDateString()}");
+            Console.WriteLine($"Age : {birth.Age}");
+
             birth.Name = "서현";
             birth.Birthday = new DateTime(1991, 6, 28);
 
