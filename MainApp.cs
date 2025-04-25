6ce203f41f8632b0a47b52cd593a2ce2529af913
Dobby-yhs <jjj1299@naver.com>
2025-04-26 02:28:18 +0900

    Properties In AbstractClass Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,35 +1,25 @@
 ﻿using System;
 
-namespace PropertiesInInterface
-{
-    interface INamedValue
+namespace PropertiesInAbstractClass
+{ 
+    abstract class Product
     {
-        // 자동 구현 프로퍼티처럼 구현이 없지만, C# 컴파일러는 인터페이스의 프로퍼티에 대해서는 자동으로 구현해주지 않습니다.
-        // 인터페이스는 어떤 구현도 가지지 않기 때문입니다.
-        string Name
+        private static int serial = 0;
+        public string SerialID
         {
-            get;
-            set;
+            get { return String.Format("{0:d5}", serial++); }
         }
 
-        string Value
+        abstract public DateTime ProductDate
         {
             get;
             set;
         }
     }
 
-    // INamedValue 인터페이스를 상속하는 NamedValue 클래스는 반드시 Name과 Value를 구현해야 합니다.
-    // 이때는 자동 구현 프로퍼티를 이용하는 것도 가능합니다.
-    class NamedValue : INamedValue
-    {
-        public string Name
-        {
-            get;
-            set;
-        }
-
-        public string Value
+    class MyProduct : Product
+    { 
+        public override DateTime ProductDate
         {
             get;
             set;
@@ -40,15 +30,13 @@ namespace PropertiesInInterface
     {
         static void Main(string[] args)
         {
-            NamedValue name = new NamedValue() { Name = "이름", Value = "홍길동" };
+            Product product_1 = new MyProduct() { ProductDate = new DateTime(2018, 1, 10) };
 
-            NamedValue height = new NamedValue() { Name = "키", Value = "182cm" };
+            Console.WriteLine("Product : {0}, Product Date : {1}", product_1.SerialID, product_1.ProductDate);
 
-            NamedValue width = new NamedValue() { Name = "몸무게", Value = "90kg" };
+            Product product_2 = new MyProduct() { ProductDate = new DateTime(2018, 2, 3) };
 
-            Console.WriteLine($"{name.Name} : {name.Value}");
-            Console.WriteLine($"{height.Name} : {height.Value}");
-            Console.WriteLine($"{width.Name} : {width.Value}");
+            Console.WriteLine("Product : {0}, Product Date : {1}", product_2.SerialID, product_2.ProductDate);
         }
     }
 }
