30a0941e990f7baecc90c174a9853789cadac651
Dobby-yhs <jjj1299@naver.com>
2025-05-02 20:14:23 +0900

    JaggedArray Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,27 +1,42 @@
 ﻿using System;
 
-namespace _2DArray
+namespace JaggedArray
 {
     class MainApp
-    { 
+    {
         static void Main(string[] args)
         {
-            int[,] arr = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[5] { 1, 2, 3, 4, 5 };
+            jagged[1] = new int[] { 10, 20, 30 };
+            jagged[2] = new int[] { 100, 200 };
 
-            for (int i = 0; i < arr.GetLength(0); ++i)
+            foreach (int[] arr in jagged)
             {
-                for (int j = 0; j < arr.GetLength(1); ++j)
+                Console.Write($"Length : {arr.Length}, ");
+                foreach (int e in arr)
                 {
-                    Console.Write($"[{i}, {j}] : {arr[i, j]}");
+                    Console.Write($"{e} ");
                 }
                 Console.WriteLine();
             }
+
             Console.WriteLine();
 
-
-            foreach(int i in arr)
+            int[][] jagged2 = new int[2][]
             {
-                Console.Write($"{i} ");
+                new int[] { 1000, 2000 },
+                new int[4] { 6, 7, 8, 9 },
+            };
+
+            foreach (int[] arr in jagged2)
+            {
+                Console.Write($"Length : {arr.Length}, ");
+                foreach (int e in arr)
+                {
+                    Console.Write($"{e} ");
+                }
+                Console.WriteLine();
             }
         }
     }
