75a293880a98f26b421bf5e6601510c33f37eb4a
Dobby-yhs <jjj1299@naver.com>
2025-05-03 15:39:23 +0900

    ArrayList Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,43 +1,51 @@
 ﻿using System;
+using System.Collections;
 
-namespace JaggedArray
+namespace UsingList
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-            int[][] jagged = new int[3][];
-            jagged[0] = new int[5] { 1, 2, 3, 4, 5 };
-            jagged[1] = new int[] { 10, 20, 30 };
-            jagged[2] = new int[] { 100, 200 };
-
-            foreach (int[] arr in jagged)
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 5; ++i)
             {
-                Console.Write($"Length : {arr.Length}, ");
-                foreach (int e in arr)
-                {
-                    Console.Write($"{e} ");
-                }
-                Console.WriteLine();
+                list.Add(i);
             }
 
+            foreach (object obj in list)
+            {
+                Console.Write($"{obj} ");
+            }
             Console.WriteLine();
 
-            int[][] jagged2 = new int[2][]
-            {
-                new int[] { 1000, 2000 },
-                new int[4] { 6, 7, 8, 9 },
-            };
 
-            foreach (int[] arr in jagged2)
+            list.RemoveAt(2);
+
+            foreach (object obj in list)
             {
-                Console.Write($"Length : {arr.Length}, ");
-                foreach (int e in arr)
-                {
-                    Console.Write($"{e} ");
-                }
-                Console.WriteLine();
+                Console.Write($"{obj} ");
             }
+            Console.WriteLine();
+
+
+            list.Insert(2, 2);
+
+            foreach (object obj in list)
+            {
+                Console.Write($"{obj} ");
+            }
+            Console.WriteLine();
+
+
+            list.Add("abc");
+            list.Add("def");
+
+            for (int i = 0;i <list.Count; ++i)
+            {
+                Console.Write($"{list[i]} ");
+            }
+            Console.WriteLine();
         }
     }
 }
