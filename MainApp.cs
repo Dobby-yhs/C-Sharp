2d728b49bbe78517b08dae6e312f66668f598d3f
Dobby-yhs <jjj1299@naver.com>
2025-05-06 01:08:21 +0900

    Enumerable Generic Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,25 +1,110 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
-namespace UsingDictionary
+namespace EnumerableGeneric
 {
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] array;
+        int position = -1;
+
+        public MyList()
+        {
+            array = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public T Current
+        {
+            get { return array[position]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return array[position];  }
+        }
+
+        public bool MoveNext()
+        {
+            if (position == array.Length - 1)
+            {
+                Reset();
+                return false;
+            }
+
+            position++;
+            return (position < array.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-           Dictionary<string, string> dic = new Dictionary<string, string>();
+            MyList<string> str_list = new MyList<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
 
-            dic["하나"] = "one";
-            dic["둘"] = "two";
-            dic["셋"] = "three";
-            dic["넷"] = "four";
-            dic["다섯"] = "five";
+            foreach(string str in str_list)
+                Console.WriteLine(str);
 
-            Console.WriteLine(dic["하나"]);
-            Console.WriteLine(dic["둘"]);
-            Console.WriteLine(dic["셋"]);
-            Console.WriteLine(dic["넷"]);
-            Console.WriteLine(dic["다섯"]);
+            Console.WriteLine();
+
+            MyList<int> int_list = new MyList<int>();
+            int_list[0] = 0;
+            int_list[1] = 1;
+            int_list[2] = 2;
+            int_list[3] = 3;
+            int_list[4] = 4;
+
+            foreach (int no in int_list)
+                Console.WriteLine(no);
         }
     }
 }
