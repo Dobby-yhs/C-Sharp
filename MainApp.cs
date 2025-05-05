13d8c7d642dff4b5ad32b28828a0b75c17f02991
Dobby-yhs <jjj1299@naver.com>
2025-05-05 17:42:14 +0900

    Generic Class Practice

--- MainApp.cs
+++ MainApp.cs
@@ -2,8 +2,41 @@
 using System.Collections;
 using static System.Console;
 
-namespace GenericMethod
+namespace GenericClass
 {
+    class MyList<T>
+    {
+        private T[] array;
+        
+        public MyList()
+        {
+            array = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get {  return array.Length; }
+        }
+    }
+
     class MainApp
     {
         static void CopyArray<T>(T[] source, T[] target)
@@ -14,24 +47,26 @@ namespace GenericMethod
 
         static void Main(string[] args)
         {
-            int[] source = { 1, 2, 3, 4, 5 };
-            int[] target = new int[source.Length];
+           MyList<string> str_list = new MyList<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
 
-            // int형으로 일반화 메서드 사용
-            CopyArray<int>(source, target);
+            for (int i = 0; i < str_list.Length; ++i)
+                Console.WriteLine(str_list[i]);
 
-            foreach (int element  in target)
-                Console.WriteLine(element);
+            Console.WriteLine();
 
-
-            string[] source2 = { "하나", "둘", "셋", "넷", "다섯" };
-            string[] target2 = new string[source2.Length];
-
-            // string형으로 일반화 메서드 사용
-            CopyArray<string>(source2, target2);
-
-            foreach (string element in target2)
-                Console.WriteLine(element);
+            MyList<int> int_list =  new MyList<int>();
+            int_list[0] = 0;
+            int_list[1] = 1;
+            int_list[2] = 2;
+            int_list[3] = 3;
+            int_list[4] = 4;
+            for (int i  =0; i < int_list.Length; ++i)
+                Console.WriteLine(int_list[i]);
         }
     }
 }
