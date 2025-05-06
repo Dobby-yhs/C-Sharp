bc2202a5a980c96f84d18dffaec7f4b745e31e7b
Dobby-yhs <jjj1299@naver.com>
2025-05-06 19:33:03 +0900

    Try Catch Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,110 +1,25 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 
-namespace EnumerableGeneric
+namespace TryCatch
 {
-    class MyList<T> : IEnumerable<T>, IEnumerator<T>
-    {
-        private T[] array;
-        int position = -1;
-
-        public MyList()
-        {
-            array = new T[3];
-        }
-
-        public T this[int index]
-        {
-            get
-            {
-                return array[index];
-            }
-            set
-            {
-                if (index >= array.Length)
-                {
-                    Array.Resize<T>(ref array, index + 1);
-                    Console.WriteLine($"Array Resized : {array.Length}");
-                }
-
-                array[index] = value;
-            }
-        }
-
-        public int Length
-        {
-            get { return array.Length; }
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return this;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this;
-        }
-
-        public T Current
-        {
-            get { return array[position]; }
-        }
-
-        object IEnumerator.Current
-        {
-            get { return array[position];  }
-        }
-
-        public bool MoveNext()
-        {
-            if (position == array.Length - 1)
-            {
-                Reset();
-                return false;
-            }
-
-            position++;
-            return (position < array.Length);
-        }
-
-        public void Reset()
-        {
-            position = -1;
-        }
-
-        public void Dispose()
-        {
-
-        }
-    }
-
     class MainApp
     {
         static void Main(string[] args)
         {
-            MyList<string> str_list = new MyList<string>();
-            str_list[0] = "abc";
-            str_list[1] = "def";
-            str_list[2] = "ghi";
-            str_list[3] = "jkl";
-            str_list[4] = "mno";
+            int[] arr = { 1, 2, 3 };
 
-            foreach(string str in str_list)
-                Console.WriteLine(str);
-
-            Console.WriteLine();
-
-            MyList<int> int_list = new MyList<int>();
-            int_list[0] = 0;
-            int_list[1] = 1;
-            int_list[2] = 2;
-            int_list[3] = 3;
-            int_list[4] = 4;
-
-            foreach (int no in int_list)
-                Console.WriteLine(no);
+            try
+            {
+                for (int i = 0; i < 5; ++i)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"예외가 발생했습니다 : {e.Message}");
+            }
+            Console.WriteLine("종료");
         }
     }
 }
