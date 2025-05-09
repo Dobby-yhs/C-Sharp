97e4b799d1a1d1b6934c19aebf2db4e302ebd846
Dobby-yhs <jjj1299@naver.com>
2025-05-09 17:36:29 +0900

    EX 13_2 Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 
-namespace EX13_1
+namespace EX13_2
 {
-    delegate int MyDelegate(int a, int b);
+    delegate int MyDelegate(int a);
+
+    class Market
+    {
+        public event MyDelegate CustomerEvent;
+      
+        public void BuySomething(int CustomerNo)
+        {
+            if (CustomerNo == 30)
+                CustomerEvent(CustomerNo);
+        }
+    }
 
     class MainApp
     {
+        static int MyHandler(int customerNo)
+        {
+            Console.WriteLine($"축하합니다! {customerNo}번째 고객 이벤트에 당첨되셨습니다.");
+            return 0; 
+        }
+
         static void Main(string[] args)
         {
-            MyDelegate Callback;
+            Market market = new Market();
+            market.CustomerEvent += new MyDelegate(MyHandler);
 
-            Callback = delegate (int a, int b)
-            {
-                return a + b;
-            };
-
-            Console.WriteLine(Callback(3, 4));
-
-            Callback = delegate (int a, int b)
-            {
-                return a - b;
-            };
-
-            Console.WriteLine(Callback(7, 5));
+            for (int customerNo = 0; customerNo < 100; customerNo += 10)
+                market.BuySomething(customerNo);
         }
     }
 }
