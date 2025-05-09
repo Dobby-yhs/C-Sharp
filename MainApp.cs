141f5b94d2cae2a85d392654a7e6dbb71ac0d143
Dobby-yhs <jjj1299@naver.com>
2025-05-09 15:55:17 +0900

    Delegate EX Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,34 +1,69 @@
 ﻿using System;
 
-namespace Delegate
+namespace UsingCallback
 {
-    delegate int MyDelegate(int a, int b);
-
-    class Calculator
-    {
-        public int Plus(int a, int b)  // 대리자는 인스턴스 메서드도 참조 가능합니다.
-        {
-            return a + b;
-        }
-
-        public static int Minus(int a, int b)  // 대리자는 정적 메서드도 참조 가능합니다.
-        {
-            return a - b;
-        }
-    }
+    delegate int Compare(int a, int b);
 
     class MainApp
     {
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+                return 1;
+            else if (a == b)
+                return 0;
+            else
+                return -1;
+        }
+
+        static void BubbleSort(int[] DataSet, Compare Comaparer)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+
+            for (i = 0; i < DataSet.Length - 1; ++i) 
+            {
+                for (j = 0; j < DataSet.Length - (i + 1); ++j) 
+                {
+                    if (Comaparer(DataSet[j], DataSet[j + 1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Calculator Calc = new Calculator();
-            MyDelegate Callback;
+            int[] array = { 3, 7, 4, 2, 10 };
 
-            Callback = new MyDelegate(Calc.Plus);
-            Console.WriteLine(Callback(3, 4));  // 메서드를 호출하듯 대리자를 사용하면, 참조하고 있는 메서드가 실행됩니다.
+            Console.WriteLine("sorting ascending...");
+            BubbleSort(array, new Compare(AscendCompare));
 
-            Callback = new MyDelegate(Calculator.Minus);
-            Console.WriteLine(Callback(7, 5));
+            for (int i = 0; i < array.Length; ++i)
+                Console.Write($"{array[i]} ");
+
+
+            int[] array2 = { 7, 2, 8, 10, 11 };
+
+            Console.WriteLine("\nSorting descending...");
+            BubbleSort(array2, new Compare(DescendCompare));
+
+            for (int i = 0; i < array2.Length; ++i)
+                Console.Write($"{array2[i]} ");
         }
     }
 }
