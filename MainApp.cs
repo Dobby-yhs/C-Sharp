39eb4a043b68ab06d8f76271e1eca79fb4fbd9e6
Dobby-yhs <jjj1299@naver.com>
2025-05-12 18:30:11 +0900

    Simple LINQ Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,19 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Ex_14_2
+namespace SimpleLinq
 {
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            int[] array = { 11, 22, 33, 44, 55 };
-
-            foreach(int a in array)
+            Profile[] arrProfile =
             {
-                Action action = () => { Console.WriteLine(a * a); };
-                action.Invoke();
-            }
+                new Profile() {Name = "AAA", Height = 186 },
+                new Profile() {Name = "BBB", Height = 158 },
+                new Profile() {Name = "CCC", Height = 172 },
+                new Profile() {Name = "DDD", Height = 178 },
+                new Profile() {Name = "EEE", Height = 171 }
+            };
+
+            var profiles = from profile in arrProfile
+                           where profile.Height < 175
+                           orderby profile.Height
+                           select new
+                           {
+                               Name = profile.Name,
+                               InchHeight = profile.Height * 0.393
+                           };
+
+            foreach (var profile in profiles)
+                Console.WriteLine($"{profile.Name}, {profile.InchHeight}");
         }
     }
 }
+
+
+
