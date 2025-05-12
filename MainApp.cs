61da4b2c845849fa291eeaa4914799998876cb6a
Dobby-yhs <jjj1299@naver.com>
2025-05-12 17:14:13 +0900

    Expression Bodied Member Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,19 +1,57 @@
 ﻿using System;
-using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace ExpressionTreeViaLambda
+namespace ExpressionBodiedMember
 {
+    class FriendList
+    {
+        private List<string> list = new List<string>();
+
+        public void Add(string name) => list.Add(name);  // list.Add() 메서드를 호출하는 식
+        public void Remove(string name) => list.Remove(name);  // list.Remove() 메서드를 호출하는 식
+        public void PrintAll()
+        {
+            foreach (var s in list)
+                Console.WriteLine(s);
+        }
+
+        public FriendList() => Console.WriteLine("FriendList()");  // 생성자의 식 구현
+        ~FriendList() => Console.WriteLine("~FriendList()");       // 소멸자의 식 구현 
+
+        // public int Capacity => list.Capacity;  // 읽기 전용 속성의 식 구현 (get 키워드 생략)
+        public int Capacity  // 속성의 식 구현
+        {
+            get => list.Capacity;
+            set => list.Capacity = value;
+        }
+
+        // public string this[int index] => list[index];  // 읽기 전용 인덱서의 식 구현(get 키워드 생략)
+        public string this[int index]  // 인덱서의 식 구현
+        {
+            get => list[index];
+            set => list[index] = value;
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            Expression<Func<int, int, int>> expression =
-                (a, b) => 1 * 2 + (a - b);
+            FriendList obj = new FriendList();
+            obj.Add("Eeny");
+            obj.Add("Meeny");
+            obj.Add("Miny");
+            obj.Remove("Eeny");
+            obj.PrintAll();
 
-            Func<int, int, int> func = expression.Compile();
+            Console.WriteLine($"{obj.Capacity}");
+            obj.Capacity = 10;
+            Console.WriteLine($"{obj.Capacity}");
 
-            // x = 7, y = 8
-            Console.WriteLine($"1 * 2 + ({7} - {8}) = {func(7, 8)}");
+            Console.WriteLine($"{obj[0]}");
+            obj[0] = "Moe";
+            obj.PrintAll();
         }
     }
 }
