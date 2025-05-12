5e74d08b9621564bfa73c19232edde10633a3a1d
Dobby-yhs <jjj1299@naver.com>
2025-05-12 19:30:21 +0900

    Inner Join Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,40 +1,57 @@
 ﻿using System;
 using System.Linq;
 
-namespace GroupBy
+namespace InnerJoin
 { 
-    class Profile
+    class Category
     {
+        public int ID { get; set; }
         public string Name { get; set; }
-        public int Height { get; set; }
     }
+
+    class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int CategoryID { get; set; }
+    }
+
 
     class MainApp
     {
         static void Main(string[] args)
         {
-            Profile[] arrProfile =
+            Category[] arrCategory =
             {
-                new Profile() {Name = "AAA", Height = 186 },
-                new Profile() {Name = "BBB", Height = 158 },
-                new Profile() {Name = "CCC", Height = 172 },
-                new Profile() {Name = "DDD", Height = 178 },
-                new Profile() {Name = "EEE", Height = 171 }
+                new Category(){ ID = 1, Name = "전자제품" },
+                new Category(){ ID = 2, Name = "도서" },
+                new Category(){ ID = 3, Name = "의류" }
             };
 
-            var listprofiles = from profile in arrProfile
-                               orderby profile.Height
-                               group profile by profile.Height < 175 into g
-                               select new { GroupKey = g.Key, Profiles = g };
-
-            foreach(var Group in listprofiles)
+            Product[] arrProduct =
             {
-                Console.WriteLine($"- 175cm 미만? : {Group.GroupKey}");
+                new Product(){ ID = 101, Name = "노트북", CategoryID = 1 },
+                new Product(){ ID = 102, Name = "키보드", CategoryID = 1 },
+                new Product(){ ID = 103, Name = "C# 프로그래밍", CategoryID = 2 },
+                new Product(){ ID = 104, Name = "C++ 프로그래밍", CategoryID = 2 },
+                new Product(){ ID = 105, Name = "청바지", CategoryID = 3 },
+                new Product(){ ID = 106, Name = "티셔츠", CategoryID = 3 },
+                new Product(){ ID = 107, Name = "알 수 없는 상품", CategoryID = 99 }
+            };
 
-                foreach(var Profile in Group.Profiles)
-                {
-                    Console.WriteLine($">>> {Profile.Name}, {Profile.Height}");
-                }
+            var listCategory = from category in arrCategory
+                               join product in arrProduct on category.ID equals product.CategoryID
+                               select new
+                               {
+                                   Name = category.Name,
+                                   ProductName = product.Name,
+                                   Id = product.ID
+                               };
+
+            foreach (var category in listCategory)
+            {
+                Console.WriteLine("카테고리 분류 : {0} / 제품 이름 : {1} / 제품 ID : {2}",
+                                   category.Name, category.ProductName, category.Id);
             }
         }
     }
