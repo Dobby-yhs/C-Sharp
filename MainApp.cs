dbb5a0f6155b1a670c5a248036f9fe4e4452e21a
Dobby-yhs <jjj1299@naver.com>
2025-05-13 17:46:34 +0900

    Basic Attribute Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,57 +1,29 @@
 ﻿using System;
-using System.Reflection;
-using System.Reflection.Emit;
 
-namespace EmitTest
+namespace BasicAttribute
 {
+    class MyClass
+    {
+        [Obsolete("OldMethod는 폐기되었습니다. NewMethod()를 이용하세요.")]
+        public void OldMethod()
+        {
+            Console.WriteLine("I'm old");
+        }
+
+        public void NewMethod()
+        {
+            Console.WriteLine("I'm new");
+        }
+    }
+
     class MainApp
     {
         static void Main(string[] args)
         {
-            // Code A
-            // Create Assembly
-            AssemblyBuilder newAssembly = 
-                AssemblyBuilder.DefineDynamicAssembly(
-                new AssemblyName("CalculatorAssembly"),
-                AssemblyBuilderAccess.Run);
+            MyClass obj = new MyClass();
 
-            // Code B
-            // Create Module
-            ModuleBuilder newModule = newAssembly.DefineDynamicModule("Calculator");
-
-            // Code C
-            // Create Class
-            TypeBuilder newType = newModule.DefineType("Sum1To100");
-
-            // Code D
-            // Create Method 
-            MethodBuilder newMethod = newType.DefineMethod(
-                "Calculate",
-                MethodAttributes.Public,
-                typeof(int),    // 반환 형식
-                new Type[0]);   // 매개변수
-
-            // Code E
-            // Create IL Commands to by executed by the method
-            ILGenerator generator = newMethod.GetILGenerator();
-
-            generator.Emit(OpCodes.Ldc_I4, 1);
-
-            for (int i = 2; i <= 100; i++)
-            {
-                generator.Emit(OpCodes.Ldc_I4, i);
-                generator.Emit(OpCodes.Add);
-            }
-
-            generator.Emit(OpCodes.Ret);
-
-            // Sum1To100 Class Emit to CLR
-            newType.CreateType();
-
-            // Generate Dynamic Instance of new type
-            object sum1To100 = Activator.CreateInstance(newType);
-            MethodInfo Calculate = sum1To100.GetType().GetMethod("Calculate");
-            Console.WriteLine(Calculate.Invoke(sum1To100, null));
+            obj.OldMethod();
+            obj.NewMethod();
         }
     }
 }
