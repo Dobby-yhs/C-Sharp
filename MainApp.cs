371e5f20f5a38188e09fe8137037f8f5711645cc
Dobby-yhs <jjj1299@naver.com>
2025-05-16 15:47:53 +0900

    COM Interop Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,50 +1,77 @@
 ﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
 
-namespace DuckTyping
+namespace COMInterop
 {
-    class Duck
-    {
-        public void  Walk()
-        { Console.WriteLine(this.GetType() + ".Walk"); }
-
-        public void Swim()
-        { Console.WriteLine(this.GetType() + ".Swim"); }
-
-        public void Quack()
-        { Console.WriteLine(this.GetType() + ".Quack"); }
-    }
-
-    class Mallard : Duck 
-    { }
-
-    class Robot
-    {
-        public void Walk()
-        { Console.WriteLine("Robot.Walk"); }
-
-        public void Swim()
-        { Console.WriteLine("Robot.Swim"); }
-
-        public void Quack()
-        { Console.WriteLine("Robot.Quack"); }
-    }
-
     class MainApp
     {
+        public static void OldWay(string[, ] data, string savePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+
+            excelApp.Workbooks.Add(Type.Missing);
+
+            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            for(int i = 0; i < data.GetLength(0); i++)
+            {
+                ((Excel.Range)workSheet.Cells[i + 1, 1]).Value2 = data[i, 0];
+                ((Excel.Range)workSheet.Cells[i + 1, 2]).Value2 = data[i, 1];
+            }
+
+            workSheet.SaveAs2(savePath + "\\shpark-book-old.xlsx",
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing);
+
+            excelApp.Quit();
+        }
+
+        public static void NewWay(string[,] data, string savePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+
+            excelApp.Workbooks.Add();
+
+            Excel.Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                workSheet.Cells[i + 1, 1] = data[i, 0];
+                workSheet.Cells[i + 1, 2] = data[i, 1];
+            }
+
+            workSheet.SaveAs2(savePath + "\\shpark-book-dynamic.xlsx");
+
+            excelApp.Quit();
+        }
+
         static void Main(string[] args)
         {
-            dynamic[] arr = {new Duck(), new Mallard(), new Robot()};
-
-            foreach(dynamic duck in arr)
+            string savePath = System.IO.Directory.GetCurrentDirectory();
+            string[,] array = new string[,]
             {
-                Console.WriteLine(duck.GetType());
+                { "좋은날", "2010" },
+                { "밤편지", "2017" },
+                { "팔레트", "2017" },
+                { "Blueming", "2019" },
+                { "에잇", "2020" },
+                { "드라마", "2021" },
+                { "라일락", "2021" },
+                { "홀씨", "2024" },
+                { "Love wins all", "2024" }
+            };
 
-                duck.Walk();
-                duck.Swim();
-                duck.Quack();
+            Console.WriteLine("Creating Excel document in old way...");
+            OldWay(array, savePath);
 
-                Console.WriteLine();
-            }
+            Console.WriteLine("Creating Excel document in new way...");
+            NewWay(array, savePath);
         }
     }
 }
