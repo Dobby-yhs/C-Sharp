972ebc511fab8d6ba7f40dba62798a620d27a268
Dobby-yhs <jjj1299@naver.com>
2025-05-23 18:59:23 +0900

    Interrupting Thread Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,38 +1,64 @@
 ﻿using System;
 using System.Threading;
 
-namespace UsingThreadState
+namespace InterruptingThread
 {
-    class MainApp
+    class SideTask
     {
-        private static void PrintThreadState(ThreadState state)
+        int count;
+
+        public SideTask(int count)
         {
-            Console.WriteLine("{0, -16} : {1}", state, (int)state);
+            this.count = count;
         }
 
+        public void KeepAlive()
+        {
+            try
+            {
+                Console.WriteLine("Running thread isn't gonna be interrupted");
+                Thread.SpinWait(1000000000);
+
+                while (count > 0)
+                {
+                    Console.WriteLine($"{count--} left");
+
+                    Console.WriteLine("Entering into WaitJoinSleep State...");
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine("Count : 0");
+            }
+            catch (ThreadInterruptedException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Clearing resource...");
+            }
+        }
+    }
+
+    class MainApp
+    {
         static void Main(string[] args)
         {
-            PrintThreadState(ThreadState.Running);                        // 000000000
-                                                                          
-            PrintThreadState(ThreadState.StopRequested);                  // 000000001
-                                                                          
-            PrintThreadState(ThreadState.SuspendRequested);               // 000000010
-                                                                          
-            PrintThreadState(ThreadState.Background);                     // 000000100
-                                                                          
-            PrintThreadState(ThreadState.Unstarted);                      // 000001000
-                                                                          
-            PrintThreadState(ThreadState.Stopped);                        // 000010000
-                                                                          
-            PrintThreadState(ThreadState.WaitSleepJoin);                  // 000100000
-                                                                          
-            PrintThreadState(ThreadState.Suspended);                      // 001000000
-                                                                          
-            PrintThreadState(ThreadState.AbortRequested);                 // 010000000
-                                                                          
-            PrintThreadState(ThreadState.Aborted);                        // 100000000
+            SideTask task = new SideTask(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
 
-            PrintThreadState(ThreadState.Aborted | ThreadState.Stopped);  // 100010000
+            Console.WriteLine("Starting thread...");
+            t1.Start();
+
+            Thread.Sleep(100);
+
+            Console.WriteLine("Interrupting thread...");
+            t1.Interrupt();
+
+            Console.WriteLine("Wating until thread stops...");
+            t1.Join();
+
+            Console.WriteLine("Finished");
         }
     }
 }
