620a182a3d8ff54fea7ed56367341f8940f014fb
Dobby-yhs <jjj1299@naver.com>
2025-05-23 18:20:15 +0900

    ThreadState Using Flags Attribute Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,72 +1,38 @@
 ﻿using System;
 using System.Threading;
 
-namespace AbortingThread
+namespace UsingThreadState
 {
-    class SideTask
-    {
-        int count;
-        CancellationToken cancellationToken;  // 취소 토큰
-
-        public SideTask(int count, CancellationToken token) // 취소 토큰을 매개변수로 받음
-        {
-            this.count = count;
-            this.cancellationToken = token;  // 취소 토큰 저장
-        }
-
-        public void KeepAlive()
-        {
-            try
-            {
-                while (count > 0)
-                {
-                    // 취소 요청 확인
-                    if (cancellationToken.IsCancellationRequested)
-                    {
-                        Console.WriteLine("취소 요청 받음. 스레드 종료 중...");
-                        // cancellationToken.ThrowIfCancellationRequested();
-                        // 예외를 던져서 종료 (선택 사항)
-                        return;  // 메서드를 빠져나가 스레드 종료
-                    }
-
-                    Console.WriteLine($"{count--} left");
-                    Thread.Sleep(10);
-                }
-                Console.WriteLine("Count : 0");
-            }
-            finally
-            {
-                Console.WriteLine("Clearing resource...");
-            }
-        }
-    }
-
     class MainApp
     {
+        private static void PrintThreadState(ThreadState state)
+        {
+            Console.WriteLine("{0, -16} : {1}", state, (int)state);
+        }
+
         static void Main(string[] args)
         {
-            // 취소 토큰 소스 생성
-            CancellationTokenSource cts = new CancellationTokenSource();
-            // 취소 토큰 얻기
-            CancellationToken token = cts.Token;
+            PrintThreadState(ThreadState.Running);                        // 000000000
+                                                                          
+            PrintThreadState(ThreadState.StopRequested);                  // 000000001
+                                                                          
+            PrintThreadState(ThreadState.SuspendRequested);               // 000000010
+                                                                          
+            PrintThreadState(ThreadState.Background);                     // 000000100
+                                                                          
+            PrintThreadState(ThreadState.Unstarted);                      // 000001000
+                                                                          
+            PrintThreadState(ThreadState.Stopped);                        // 000010000
+                                                                          
+            PrintThreadState(ThreadState.WaitSleepJoin);                  // 000100000
+                                                                          
+            PrintThreadState(ThreadState.Suspended);                      // 001000000
+                                                                          
+            PrintThreadState(ThreadState.AbortRequested);                 // 010000000
+                                                                          
+            PrintThreadState(ThreadState.Aborted);                        // 100000000
 
-            SideTask task = new SideTask(100, token); // 취소 토큰 전달
-            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
-            t1.IsBackground = false;
-
-            Console.WriteLine("Starting thread...");
-            t1.Start();
-
-            Thread.Sleep(100);
-
-            Console.WriteLine("Requesting cancellation...");
-            cts.Cancel(); // 취소 요청
-
-
-            Console.WriteLine("Waiting until thread stops...");
-            t1.Join();
-
-            Console.WriteLine("Finished");
+            PrintThreadState(ThreadState.Aborted | ThreadState.Stopped);  // 100010000
         }
     }
 }
