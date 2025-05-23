540f786e8e49a5ae431ff3ffb0da894c0275e9fe
Dobby-yhs <jjj1299@naver.com>
2025-05-23 17:29:48 +0900

    Implement Trhead Practice

--- MainApp.cs
+++ MainApp.cs
@@ -1,60 +1,38 @@
 ﻿using System;
-using System.IO;
-using FS = System.IO.FileStream;
+using System.Threading;
 
-
-namespace ManualBinarySerialization
+namespace BasicThread
 {
-    class NameCard
-    {
-        public string Name;
-        public string Phone;
-        public int Age;
-
-        public int HaveMoney;
-
-        public NameCard() { }
-
-        public NameCard(string name, string phone, int age)
-        {
-            Name = name;
-            Phone = phone;
-            Age = age;
-        }
-    }
-
     class MainApp
     {
+        static void DoSomething()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"DoSomething : {i}");
+                // Sleep() 메서드를 만나면 인수(10)만큼 CPU 사용을 멈춥니다.
+                // 이때 인수 단위는 밀리초입니다.
+                Thread.Sleep(10);
+            }
+        }
+
         static void Main(string[] args)
         {
-            string binaryFilePath = "a.dat";
+            Thread t1 = new Thread(new ThreadStart(DoSomething));
 
-            NameCard originalNc = new NameCard("홍길동", "010-123-4567", 33);
+            Console.WriteLine("Starting thread...");
+            t1.Start();
 
-            using (Stream fs = new FS(binaryFilePath, FileMode.Create))
-            using (BinaryWriter bw = new BinaryWriter(fs))
+            for (int i = 0; i < 5; i++) 
             {
-                bw.Write(originalNc.Name);
-                bw.Write(originalNc.Phone);
-                bw.Write(originalNc.Age);
-
-                Console.WriteLine($"'{binaryFilePath}'에 이진 직렬화 완료.");
+                Console.WriteLine($"Main : {i}");
+                Thread.Sleep(10);
             }
 
-            using (Stream fs = new FS(binaryFilePath, FileMode.Open))
-            using (BinaryReader br = new BinaryReader(fs))
-            {
-                NameCard deserializedNc = new NameCard();
+            Console.WriteLine("Wating until thread stops...");
+            t1.Join();
 
-                deserializedNc.Name = br.ReadString();
-                deserializedNc.Phone = br.ReadString();
-                deserializedNc.Age = br.ReadInt32();
-
-                Console.WriteLine($"'{binaryFilePath}'에서 이진 역직렬화 완료.");
-                Console.WriteLine($"Name : {deserializedNc.Name}");
-                Console.WriteLine($"Phone : {deserializedNc.Phone}");
-                Console.WriteLine($"Age : {deserializedNc.Age}");
-            }
+            Console.WriteLine("Finished");
         }
     }
 }
